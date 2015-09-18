277d9d031f9869b5ced56daea46f4c7c51cd51f0
Kim Dilling <kimdilling@gmail.com>
2015-09-18 15:11:28 -0500

    mvp

--- d05/starter/app.rb
+++ d05/starter/app.rb
@@ -3,30 +3,41 @@ require 'sinatra/reloader'
 
 enable :sessions
 
-#before do 
-
-#end
-
-
-# routes
-get '/' do
- 	#link the form
-
-erb :index	
+before do 
+	session[:message_text] ||= []
 end
 
 
-# post '/post' do
-# 	#{if less than 142 charcters then post}
-# 	@messages = [gets.chomp]
-# 	 if messages.length < 142 do
-# 	 	submit
-# 	 end
-	
-# 	# else
-# 	# 	submit "Error Message - Over 142 Characters"
-# 	# end
+# routes
+get '/index' do
+ 	erb :index	
+end
 
-# 	#erb :post
-# end
+
+post '/post' do
+	#if less than 142 charcters then post
+	
+	message_text = params[:message_text]
+	session[:message_text] << message_text
+	@messages = session[:message_text].reverse
+	
+	 # session[@messages] = @new_messages
+	 #how to save data from previous session
+
+
+	# if message_text.length < 142 
+	#  	@message_text = message_text
+	#  	@messages << @message_text
+	# else
+	# @alert = "Error Message - Over 142 Characters"
+	# end
+
+	erb :post
+end
+
+get '/restart' do
+    session.clear
+
+    redirect '/'
+end
 
